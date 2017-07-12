06e410d7a01b618d5218c6281f9d31bdd84a895d
Chaz Henricks <chazhenricks@gmail.com>
2017-07-11 20:39:38 -0500

    setting up list

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace nickleback
 {
@@ -6,7 +7,7 @@ namespace nickleback
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<(string, string)> goodSongs = new List<(string, string)>();
         }
     }
 }
